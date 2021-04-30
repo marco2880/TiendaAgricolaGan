42c9bf5bedf91a1c232c764379a68d0ae28f6ce4
estudio <marco2880@gmail.com>
2021-04-30 09:22:37 -0500

    agrego intent a registro de usuarios

--- app/src/main/java/tiendaagricolagan/virtual/peru/marco/AccesoActivity.java
+++ app/src/main/java/tiendaagricolagan/virtual/peru/marco/AccesoActivity.java
@@ -6,28 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class AccesoActivity extends AppCompatActivity {
 
+    TextView registro_acceso;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acceso);
+        registro_acceso = findViewById(R.id.registro_acceso);
+        registro_acceso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(AccesoActivity.this,RegisterUser.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 
-    public void sendMessage(View view)
-    {
-        Intent intent= new Intent(this, RegisterUser.class);
-
-        startActivity(intent);
-        finish();
 
 
 
-    }
 
 
 
--- app/src/main/java/tiendaagricolagan/virtual/peru/marco/RegisterUser.java
+++ app/src/main/java/tiendaagricolagan/virtual/peru/marco/RegisterUser.java
@@ -2,13 +2,27 @@ package tiendaagricolagan.virtual.peru.marco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterUser extends AppCompatActivity {
+    Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+
+
+
+
+
+
+
     }
 }
